4d1a80e23e0d73ba938689d376813cf688fb9d12
Kajalror <krulyan90@gmail.com>
2024-05-22 13:26:52 +0530

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,46 +1,23 @@
 
+import React from 'react'
+import Link from 'next/link'
 
-import { useClient } from 'next/client';
-import { Routes, Route  } from 'react-router-dom';
-import UserPage from "./user/page";
-import Form from '@/components/form/page';
-
-
-const ProtectedRoute = ({children, auth= false })=>{
-  const Router = useClient();
-  const isLoggedIn = localStorage.getItem('user:tokens')!== null  || false;
-      console.log("isLoggedIn:>>>>", isLoggedIn );
-
-  if(isLoggedIn && auth){
-    console.log("authentication---signed_in");
-    return Router.push("/users/sign_in" )
-  }
-  else if(isLoggedIn && ['/users/sign_in', '/users/sign_up']?.includes(window.location.pathname) ){
-    console.log("user navigate : >> ");
-    return Router.push("/user" )
-  }
-  return children;
-}
-
-export default function Home() {
+const page = () => {
   return (
-   
-        <Routes>
-          <Route path='/user' element={
-          <ProtectedRoute auth = { true }>
-            <UserPage />
-          </ProtectedRoute>
-          } />
-          <Route path='/users/sign_in' element={
-            <ProtectedRoute>
-              <Form isSignInPage={ true }/>
-            </ProtectedRoute>
-          } />
-          <Route path='/users/sign_up' element={
-              <ProtectedRoute>
-            <Form isSignInPage={ false }/>
-          </ProtectedRoute>
-          } />
-        </Routes>
-  );
+    <>
+    <div className=' border w-full h-[99vh] bg-[#dfe6ed] flex justify-center align-center text-center '>
+        <div className='border flex flex-col justify-center border w-[50%] h-[40vh] p-3  pt-4 mt-5 '  style={{fontSize:"25px"}}> 
+           <h1 className='p-5'> Welcome To ChatApp </h1>
+          <div className='p-3'>
+            <Link href="/users/sign_in"> <button className='bg-[#2380d9] p-2 text-red-800'>Login chat </button> </Link>
+          </div>
+          <div className='p-3'>
+            <Link href="/users/sign_up"> <button className='bg-[#2380d9] p-2 text-red-800'>  Register </button> </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
+
+export default page
